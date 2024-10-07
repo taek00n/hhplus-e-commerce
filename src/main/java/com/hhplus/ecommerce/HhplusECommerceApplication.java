51259b68dfc365622bb83305a042d522fd022802
taek00n <kimth0526@naver.com>
2024-10-07 20:51:13 +0900

    make repo

---
+++ src/main/java/com/hhplus/ecommerce/HhplusECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/ecommerce/HhplusECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
