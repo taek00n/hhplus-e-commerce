dbf71aa7da315a44610a0860fd911be84ba8e327
taek00n <kimth0526@naver.com>
2024-10-15 22:29:28 +0900

    fix: 구조 수정 및 명칭 변경

--- src/main/java/com/hhplus/ecommerce/infrastructure/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hhplus.ecommerce.infrastructure;
-
-import com.hhplus.ecommerce.domain.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
-}
--- src/main/java/com/hhplus/ecommerce/presentation/OrderController.java
+++ src/main/java/com/hhplus/ecommerce/presentation/OrderController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/orders")
 @RestController
-public class OrdersController {
+public class OrderController {
 
 }
