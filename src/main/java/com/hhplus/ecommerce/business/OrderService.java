b95566c99b566642da5ce9badcc3c4b86b39a84e
taek00n <kimth0526@naver.com>
2024-10-17 02:21:24 +0900

    fest, test: 상위 상품 조회 개발 및 테스트

--- src/main/java/com/hhplus/ecommerce/business/OrderService.java
+++ src/main/java/com/hhplus/ecommerce/business/OrderService.java
@@ -1,9 +1,14 @@
 package com.hhplus.ecommerce.business;
 
+import com.hhplus.ecommerce.domain.Item;
 import com.hhplus.ecommerce.domain.Order;
 import com.hhplus.ecommerce.infrastructure.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +25,11 @@ public class OrderService {
 
         return orderRepository.getOrder(ordersId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 주문은 존재하지않습니다."));
+    }
+
+    public List<Item> findTopItems() {
+        LocalDateTime endDateTime = LocalDate.now().atStartOfDay();
+        LocalDateTime startDateTime = endDateTime.minusDays(3);
+        return orderRepository.findTopItems(startDateTime, endDateTime);
     }
 }
