2934b54451996810d71fff1aa59f9bf5a59a5c02
kim taehyun <140230923+taek00n@users.noreply.github.com>
2024-10-17 02:25:26 +0900

    STEP07: 주요 비즈니스 로직 개발 및 단위 테스트 작성 (#8)
    * fest, test: 상위 상품 조회 개발 및 테스트

--- src/main/java/com/hhplus/ecommerce/infrastructure/OrderRepository.java
+++ src/main/java/com/hhplus/ecommerce/infrastructure/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.hhplus.ecommerce.infrastructure;
 
+import com.hhplus.ecommerce.domain.Item;
 import com.hhplus.ecommerce.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -10,4 +14,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Order createOrder(Order order);
 
     Optional<Order> getOrder(Long ordersId);
+
+    @Query(value = "select d.item, count(*) from Order o left join o.orderDetails d where o.orderDate between :startDateTime and :endDateTime group by d.item order by count(*) desc")
+    List<Item> findTopItems(LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
--- src/test/java/com/hhplus/ecommerce/business/OrderServiceTest.java
+++ src/test/java/com/hhplus/ecommerce/business/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package com.hhplus.ecommerce.business;
 
 import com.hhplus.ecommerce.common.constant.OrderStatus;
+import com.hhplus.ecommerce.domain.Item;
 import com.hhplus.ecommerce.domain.Order;
 import com.hhplus.ecommerce.domain.User;
 import com.hhplus.ecommerce.infrastructure.OrderRepository;
@@ -12,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -61,5 +64,32 @@ class OrderServiceTest {
         //then
         assertNotNull(resultOrder);
         assertEquals(mockOrder.getOrderId(), resultOrder.getOrderId());
+    }
+
+    @Test
+    @DisplayName("주문_상위5개_조회")
+    void findTopItems() {
+        // given
+        LocalDateTime endDateTime = LocalDate.now().atStartOfDay();
+        LocalDateTime startDateTime = endDateTime.minusDays(3);
+
+        Item mockItem1 = buildItem(1L, "청바지1", 10000, 90);
+        Item mockItem2 = buildItem(2L, "청바지2", 20000, 90);
+        Item mockItem3 = buildItem(3L, "청바지3", 30000, 90);
+        Item mockItem4 = buildItem(4L, "청바지4", 40000, 90);
+        Item mockItem5 = buildItem(5L, "청바지5", 50000, 90);
+        Item mockItem6 = buildItem(6L, "청바지6", 60000, 90);
+        List<Item> mockTopItemList = List.of(mockItem1, mockItem2, mockItem3, mockItem4, mockItem5);
+        when(orderRepository.findTopItems(startDateTime, endDateTime)).thenReturn(mockTopItemList);
+        //when
+        List<Item> resultTopItem = orderService.findTopItems();
+        //then
+        assertNotNull(resultTopItem);
+        assertEquals(mockTopItemList.size(), resultTopItem.size());
+        assertEquals(mockTopItemList.get(0), resultTopItem.get(0));
+    }
+
+    private Item buildItem(Long itemId, String itemName, int itemPrice, int itemStock) {
+        return Item.builder().itemId(itemId).itemName(itemName).itemPrice(itemPrice).itemStock(itemStock).build();
     }
 }
