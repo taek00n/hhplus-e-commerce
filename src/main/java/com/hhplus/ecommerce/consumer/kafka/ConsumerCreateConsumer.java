771503cf26282ce3de3a15f1c0287190a1b26178
taek00n <kimth0526@naver.com>
2024-11-21 01:13:11 +0900

    feat: kafka - consumer, producer 연동

--- src/main/java/com/hhplus/ecommerce/consumer/kafka/ConsumerCreateConsumer.java
+++ src/main/java/com/hhplus/ecommerce/consumer/kafka/ConsumerCreateConsumer.java
@@ -1,13 +1,15 @@
 package com.hhplus.ecommerce.consumer.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ConsumerCreateConsumer {
 
     @KafkaListener(topics = "kafka-test", groupId = "test")
     public void listener(Long userId) {
-
+        log.info(userId.toString());
     }
 }
